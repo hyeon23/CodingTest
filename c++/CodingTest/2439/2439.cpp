88b0e7db49fb68822a3cec8cfa7f89f61c1f7742
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-03-02 15:50:08 +0900

    Clear Update

---
+++ c++/CodingTest/2439/2439.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int N;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int k = N - 1; k > i; k--) {
+			cout << " ";
+		}
+
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << '\n';
+	}
+}
