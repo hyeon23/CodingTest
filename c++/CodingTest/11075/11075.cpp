75a828d5c60e4fdcdc368c26d7d95f3a0cf51c56
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2023-03-03 16:51:18 +0900

    Update

---
+++ c++/CodingTest/11075/11075.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define MOD 10007
+
+int main()
+{
+    int N;
+    int sum = 0;
+    int arr[1001][10] = { 0, };
+
+    cin >> N;
+
+    for (int i = 0; i < 10; i++) {
+        arr[1][i] = 1;
+    }
+
+    for (int i = 2; i <= N; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (j == 0) {
+                arr[i][j] = 1;
+            }
+            else {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
+            }
+
+            arr[i][j] %= MOD;
+        }
+    }
+
+    for (int i = 0; i < 10; i++) {
+        sum += arr[N][i];
+    }
+
+    sum %= MOD;
+
+    cout << sum << endl;
+
+    return 0;
+}
