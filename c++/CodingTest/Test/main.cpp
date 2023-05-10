61d9bf8ab90b3978e8950604dbe61efe39004b25
hyeon23 <urim2270@naver.com>
2023-05-10 17:29:24 +0900

    C++ 독학중9

--- c++/CodingTest/Test/main.cpp
+++ c++/CodingTest/Test/main.cpp
@@ -1,40 +1,19 @@
 #include <iostream>
 
-class Marine {
-	int hp;
-	int coord_x, coord_y;
-	int damage;
-	bool is_dead;
+class A {
+	mutable int data_;
 
 public:
-	Marine();
-	Marine(int x, int y);
-	
-public:
-	int attack();
-	void be_attacked(int damage_earn);
-	void move(int x, int y);
-	void show_status();
+	A(int data) : data_(data){}
+	void DoSomething(int x) const {
+		data_ = x;//°¡´É!
+	}
+
+	void PrintData() const { std::cout << "data: " << data_ << std::endl; }
 };
 
-Marine::Marine() : hp(50), coord_x(0), coord_y(0), damage(50), is_dead(false) {}
-Marine::Marine(int x, int y) : coord_x(x), coord_y(y), hp(50), damage(5), is_dead(false) {}
-
-void Marine::move(int x, int y) { coord_x = x; coord_y = y; }
-int Marine::attack() { return damage; }
-void Marine::be_attacked(int damage_earn) {
-	hp -= damage_earn;
-	if (hp <= 0) is_dead = true;
-}
-void Marine::show_status() {
-	std::cout << "***Marine***" << std::endl;
-	std::cout << "Location : ( " << coord_x << " , " << coord_y << " ) " << std::endl;
-	std::cout << " HP : " << hp << std::endl;
-}
-
 int main() {
-	Marine marine1(2, 3);
-	Marine marine2(3, 5);
-	marine1.show_status();
-	marine2.show_status();
+	A a(10);
+	a.DoSomething(3);
+	a.PrintData();
 }
