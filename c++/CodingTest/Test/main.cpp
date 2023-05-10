d34b3b515429b6f4bb57787df91c7bf904930598
hyeon23 <urim2270@naver.com>
2023-05-10 13:56:57 +0900

    C++ 독학중8

--- c++/CodingTest/Test/main.cpp
+++ c++/CodingTest/Test/main.cpp
@@ -1,53 +1,40 @@
-#include <string.h>
 #include <iostream>
 
-class Photon_Cannon {
-	int hp, sheld;
+class Marine {
+	int hp;
 	int coord_x, coord_y;
 	int damage;
-	char* name;
+	bool is_dead;
 
 public:
-	Photon_Cannon(int x, int y);
-	Photon_Cannon(int x, int y, const char* cannon_name);
-	~Photon_Cannon();
-
+	Marine();
+	Marine(int x, int y);
+	
 public:
+	int attack();
+	void be_attacked(int damage_earn);
+	void move(int x, int y);
 	void show_status();
 };
 
-Photon_Cannon::Photon_Cannon(int x, int y) {
-	hp = sheld = 100;
-	coord_x = x;
-	coord_y = y;
-	damage = 20;
+Marine::Marine() : hp(50), coord_x(0), coord_y(0), damage(50), is_dead(false) {}
+Marine::Marine(int x, int y) : coord_x(x), coord_y(y), hp(50), damage(5), is_dead(false) {}
 
-	name = NULL;
+void Marine::move(int x, int y) { coord_x = x; coord_y = y; }
+int Marine::attack() { return damage; }
+void Marine::be_attacked(int damage_earn) {
+	hp -= damage_earn;
+	if (hp <= 0) is_dead = true;
 }
-Photon_Cannon::Photon_Cannon(int x, int y, const char* cannon_name) {
-	hp = sheld = 100;
-	coord_x = x;
-	coord_y = y;
-	damage = 20;
-
-	name = new char[strlen(cannon_name) + 1];
-	strcpy(name, cannon_name);
-
-}
-Photon_Cannon::~Photon_Cannon() {
-	if (name) delete[] name;
-}
-void Photon_Cannon::show_status() {
-	std::cout << "Photon Cannon :: " << name << std::endl;
-	std::cout << " Location : ( " << coord_x << " , " << coord_y << " ) "
-		<< std::endl;
+void Marine::show_status() {
+	std::cout << "***Marine***" << std::endl;
+	std::cout << "Location : ( " << coord_x << " , " << coord_y << " ) " << std::endl;
 	std::cout << " HP : " << hp << std::endl;
 }
+
 int main() {
-	Photon_Cannon pc1(3, 3, "Cannon");
-	Photon_Cannon pc2 = pc1;
-
-	pc1.show_status();
-	pc2.show_status();
+	Marine marine1(2, 3);
+	Marine marine2(3, 5);
+	marine1.show_status();
+	marine2.show_status();
 }
-
