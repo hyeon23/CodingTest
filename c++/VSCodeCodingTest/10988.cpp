51c83a2d5a93d71467c28e17cc1ac17ae2248359
hyeon23 <urim2270@naver.com>
2023-05-19 17:58:14 +0900

    C++ 백준문제 풀이7

--- c++/VSCodeCodingTest/10988.cpp
+++ c++/VSCodeCodingTest/10988.cpp
@@ -4,30 +4,43 @@ using namespace std;
 
 int main()
 {
-    cout.tie(NULL);
-    cin.tie(NULL);
-    std::ios_base::sync_with_stdio(false);
+    //sol 1
+    // cout.tie(NULL);
+    // cin.tie(NULL);
+    // std::ios_base::sync_with_stdio(false);
 
+    // string s;
+
+    // //한 단어로 이뤄진 경우 펠린드롬을 체크하기 위해 1로 설정
+    // int check = 1;
+
+    // cin >> s;
+
+    // for (int i = 0; i < s.length() / 2; ++i)
+    // {
+    //     if(s[i] != s[s.length() - 1 - i]){
+    //         check = 0;
+    //         break;
+    //     }
+    //     else{
+    //         if (i == s.length() / 2 - 1)
+    //             check = 1;
+    //     }
+    // }
+
+    // cout << check;
+
+    //sol 2
     string s;
-
-    //한 단어로 이뤄진 경우 펠린드롬을 체크하기 위해 1로 설정
-    int check = 1;
-
     cin >> s;
 
-    for (int i = 0; i < s.length() / 2; ++i)
-    {
-        if(s[i]-'0' != s[s.length() - 1 - i]-'0'){
-            check = 0;
-            break;
-        }
-        else{
-            if (i == s.length() / 2 - 1)
-                check = 1;
+    for(int i = 0; i < s.length() / 2; ++i){
+        if(s[i] != s[s.length() - i - i]){
+            cout << "0" << endl;
+            return 0;
         }
     }
 
-    cout << check;
-
+    cout << "1" <<endl;
     return 0;
 }
