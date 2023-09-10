0097267293cda2f71bedf903cc30ccaa69696513
hyeon <urim2270@naver.com>
2023-09-10 12:37:39 +0900

    C++ Beakjoon#32

--- c++/VSCodeCodingTest/1296.cpp
+++ c++/VSCodeCodingTest/1296.cpp
@@ -1,37 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int N;
-    string yundo, name;
-    cin >> yundo >> N;
-    vector<pair<int, string>> ans;
-
-    vector<int> str(yundo.size(), 0);
-
-    for(int i = 0; i < yundo.size(); ++i){
-        str[i]++;
+bool cmp(pair<string, int> A, pair<string, int> B){
+    if(A.second == B.second){
+        return A.first < B.first;
     }
+    return A.second > B.second;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int N;
+    string mj, str;
+    cin >> mj;
+
+    vector<int> mjcnt(mj.size(), 0);
+    //mj의 L, O, V, E 숫자 카운팅
+
+    for(int i = 0; i < mj.size(); ++i){
+        if(mj[i] == 'L'){
+            mjcnt[0]++;
+        }
+        else if(mj[i] == 'O'){
+            mjcnt[1]++;
+        }
+        else if(mj[i] == 'V'){
+            mjcnt[2]++;
+        }
+        else if(mj[i] == 'E'){
+            mjcnt[3]++;
+        }
+    }
+
+
+    cin >> N;
+    vector<pair<string, long long>> ans(N);
 
     for(int i = 0; i < N; ++i){
+        cin >> str;//세는 단어
+        vector<int> strcnt(str.size(), 0);
 
-        cin >> name;
-
-        //확률 계산
-        for(int i = 0; i < name.size(); ++i){
-            vector<int> str2(yundo.size(), 0);
-            for(int j = 0; j < yundo.size(); ++j){
-                if(name[i] == yundo[j]){
-                    str2[j]++;
-                    break;
-                }
+        for(int j = 0; j < str.size(); ++j){
+            if(str[j] == 'L'){
+                strcnt[0]++;
             }
-            
+            else if(str[j] == 'O'){
+                strcnt[1]++;
+            }
+            else if(str[j] == 'V'){
+                strcnt[2]++;
+            }
+            else if(str[j] == 'E'){
+                strcnt[3]++;
+            }
         }
 
-        //횟수, 이름 저장 벡터
+        vector<int> tmp(4, 0);
 
+        for(int j = 0; j < 4; ++j){
+            tmp[j] = (mjcnt[j] + strcnt[j]);
+        }
+
+        long long sum = 1;
+
+        for(int j = 0; j < 4; ++j){
+            for(int k = j + 1; k < 4; ++k){
+                sum *= (tmp[j] + tmp[k]);
+            }
+        }
+
+        ans[i] = {str, sum % 100};
     }
 
-    return 0;
+    sort(ans.begin(), ans.end(), cmp);
+
+    cout << ans[0].first;
 }
