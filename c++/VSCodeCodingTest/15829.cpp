157f4eff79a38737272ecee05d5c18f5f770485d
hyeon <urim2270@naver.com>
2023-09-24 14:29:44 +0900

    C++ Beakjoon#43

---
+++ c++/VSCodeCodingTest/15829.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //해시함수 -> 자료의 저장과 탐색에 쓰임
+    int L;
+    cin >> L;
+
+    string str;
+    cin >> str;
+
+    long long sum = 0;
+
+    for(long long i = 0; i < L; ++i){
+        long long ai = (str[i] - 'a' + 1);
+
+        long long ri = 1;
+
+        for(int j = 0; j < i; ++j){
+            ri = ((ri % 1234567891) * 31) % 1234567891;
+        }
+        
+        sum += ((ai % 1234567891) * (ri % 1234567891)) % 1234567891;
+    }
+
+    cout << sum % 1234567891;
+
+    return 0;
+}
