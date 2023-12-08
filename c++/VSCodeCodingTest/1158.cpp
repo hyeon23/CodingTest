c6c8fa0ada6bd1f7665a3b31d095df5fb8042d64
hyeon23 <urim2270@naver.com>
2023-12-08 12:16:28 +0900

    LinkedList update

--- c++/VSCodeCodingTest/1158.cpp
+++ c++/VSCodeCodingTest/1158.cpp
@@ -8,20 +8,44 @@ int main(){
     for(int i = 1; i <= N; ++i){
         q.push(i);
     }
-    long long cnt = 0;
     cout << '<';
     while(!q.empty()){
-        ++cnt;
-        if(cnt % K == 0){
-            if(q.size() == 1) cout << q.front();
-            else cout << q.front() << ", ";
-            q.pop();
-
-        }
-        else{
+        for(int i = 0; i < K - 1; ++i){
             q.push(q.front());
             q.pop();
         }
+        cout << q.front();
+        q.pop();
+
+        if(q.size()) cout << ", ";
     }
    cout << '>'; 
 }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int N, K;
+//     cin >> N >> K;
+//     list<int> q;
+//     for(int i = 1; i <= N; ++i){
+//         q.push_front(i);
+//     }
+//     long long cnt = 0;
+//     cout << '<';
+//     while(!q.empty()){
+//         ++cnt;
+//         if(cnt % K == 0){
+//             if(q.size() == 1) cout << q.back();
+//             else cout << q.back() << ", ";
+//             q.pop_back();
+
+//         }
+//         else{
+//             q.push_front(q.back());
+//             q.pop_back();
+//         }
+//     }
+//    cout << '>'; 
+// }
