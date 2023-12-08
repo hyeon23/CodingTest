2ea3d1b424d43c5da5a0515d4dceb2edd7843f7b
hyeon23 <urim2270@naver.com>
2023-12-08 16:20:42 +0900

    I will commit

--- c++/VSCodeCodingTest/1874.cpp
+++ c++/VSCodeCodingTest/1874.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
 #include <stack>
 #include <vector>
-
 using namespace std;
-
 int main(){
     stack<int> s;
     vector<char> result;
     int cnt = 1;
     int n;
     cin >> n;
-
     for(int i = 0; i < n; ++i){
         int x;
         cin >> x;
-
         while(cnt <= x){
-            s.push(cnt);
-            cnt += 1;
+            s.push(cnt++);
             result.push_back('+');
         }
-
         if(s.top() == x){
             s.pop();
             result.push_back('-');
@@ -30,7 +24,6 @@ int main(){
             return 0;
         }
     }
-
     for(int i = 0; i < result.size(); ++i){
         cout << result[i] << '\n';
     }
