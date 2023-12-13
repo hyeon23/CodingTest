ce2208449b363761cdb408487cde111e4d2484e1
hyeon23 <urim2270@naver.com>
2023-12-14 01:42:14 +0900

    boj 10026.cpp update

--- c++/VSCodeCodingTest/1697.cpp
+++ c++/VSCodeCodingTest/1697.cpp
@@ -5,48 +5,50 @@ vector<int> dot(100001, 0);
 int N, K;
 int dx[2] = {-1, 1};
 
-void bfs(int cur){
-    queue<int> q;
+void bfs(int cur, int num){
+    queue<pair<int, int>> q;
     dot[cur] = 1;
-    q.push(cur);
+    q.push({cur, 0});
 
     while(!q.empty()){
-        int cx = q.front();
-        q.pop();
+        int cx = q.front().first;
+        int cn = q.front().second;
 
+        if(cx == K){
+            cout << cn;
+            return;
+        }
+
+        q.pop();
         if(cx == K){
             cout << dot[cx];
             return;
         }
         int nx;
-        //+1이동
-        nx = cx + 1;
-        if(nx < 0 || nx >= 100001) continue;
-        if(dot[nx] == 1) continue;
-        dot[nx] = 1;
-
-        //-1이동
-        nx = cx - 1;
-        if(nx < 0 || nx >= 100001) continue;
-        if(dot[nx] == 1) continue;
-        dot[nx] = 1;
-
-        //순간이동
-        nx = cx * 2;
-        if(nx < 0 || nx >= 100001) continue;
-        if(dot[nx] == 1) continue;
-        dot[nx] = 1;
-        
-        if(nx < 0 || nx >= 100001) continue;
-        if(dot[nx] == 1) continue;
-        dot[nx] = 1;
-        
+        for(int i = 0; i < 3; ++i){
+            //+1 이동 -1 이동
+            if(i != 2){
+                nx = cx + dx[i];
+                if(nx < 0 || nx >= 100001) continue;
+                if(dot[nx] == 1) continue;
+                dot[nx] = 1;
+                q.push({nx, cn+1});
+            }
+            else{
+                //순간이동
+                nx = cx * 2;
+                if(nx < 0 || nx >= 100001) continue;
+                if(dot[nx] == 1) continue;
+                dot[nx] = 1;
+                q.push({nx, cn+1});
+            }
+        }
     }
     return;
 }
 
 int main(){
     cin >> N >> K;
-    bfs(N);
+    bfs(N, 0);
     return 0;
 }
