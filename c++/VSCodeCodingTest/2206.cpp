17652ca28edcaee14511117cd2099f03aa3b8e55
hyeon23 <urim2270@naver.com>
2023-12-15 21:04:53 +0900

    boj update

--- c++/VSCodeCodingTest/2206.cpp
+++ c++/VSCodeCodingTest/2206.cpp
@@ -1,58 +1,71 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits> // for INT_MAX
 using namespace std;
+
 int N, M, ans = INT_MAX;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 vector<vector<char>> board(1001, vector<char>(1001, '1'));
-bool visited[1001][1001] = {false,};
+bool visited[1001][1001][2] = {false,};
 
-struct DATA{
+struct DATA {
     int x;
     int y;
     int dist;
-    bool b;
+    int b; // 0: 벽을 부수지 않음, 1: 벽을 부숨
 };
 
-int main(){
+int main() {
+    // Input
     cin >> N >> M;
     for(int i = 0; i < N; ++i){
         for(int j = 0; j < M; ++j){
             cin >> board[i][j];
         }
     }
+
     bool trigger = false;
     queue<DATA> que;
-    visited[0][0] = true;
-    que.push({0, 0, 1, true});
-    while(!que.empty()){
+    visited[0][0][0] = true;
+    que.push({0, 0, 1, 0});
+
+    // BFS
+    while (!que.empty()) {
         DATA cur = que.front();
-        if(cur.x == N-1 && cur.y == M-1){
+        if (cur.x == N - 1 && cur.y == M - 1) {
             ans = min(ans, cur.dist);
             trigger = true;
         }
         que.pop();
-        for(int i = 0; i < 4; ++i){
+
+        for (int i = 0; i < 4; ++i) {
             int nx = cur.x + dx[i];
             int ny = cur.y + dy[i];
-            if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-            if(visited[nx][ny]) continue;
-            if(board[nx][ny] == '1'){//현재 벽을 만난 경우
-                if(cur.b){//현재 벽을 부술 수 있다면
-                    //벽을 부숨
-                    visited[nx][ny] = true;
-                    que.push({nx, ny, cur.dist+1, false});
+
+            // Check boundary
+            if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+
+            // Check visited
+            if (visited[nx][ny][cur.b]) continue;
+
+            if (board[nx][ny] == '1') { // 현재 벽을 만난 경우
+                if (cur.b == 0) { // 벽을 부수지 않은 상태라면
+                    // 벽을 부숨
+                    visited[nx][ny][1] = true;
+                    que.push({nx, ny, cur.dist + 1, 1});
                 }
-                else{//벽을 부술 수 없다면
-                    continue;
-                }
-            }
-            else if(board[nx][ny] == '0'){//그 외 -> 일반 BFS처럼 수행
-                visited[nx][ny] = true;
-                que.push({nx, ny, cur.dist+1, cur.b});
+            } else { // 그 외 -> 일반 BFS처럼 수행
+                visited[nx][ny][cur.b] = true;
+                que.push({nx, ny, cur.dist + 1, cur.b});
             }
         }
     }
-    if(trigger) cout << ans;
+
+    // Output
+    if (trigger) cout << ans;
     else cout << -1;
+
     return 0;
 }
--- c++/VSCodeCodingTest/9466.cpp
+++ c++/VSCodeCodingTest/9466.cpp
@@ -1,7 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+int T, n;
 int main(){
-    
+    cin >> T;
+    while(T--){
+        cin >> n;
+        vector<int> ans;
+        vector<int> board(n+1, 0);
+        vector<bool> visited(n+1, false);
+        for(int i = 1; i <= n; ++i){
+            cin >> board[i];
+        }
+        for(int i = 1; i <= n; ++i){
+            stack<int> stk;
+            visited[i] = true;
+            stk.push(i);
+            while(!stk.empty()){
+                int cx = stk.top();
+                stk.pop();
+                int nx = board[cx];
+                if(cx < 1 || cx > n || visited[nx]) continue;
+                if(i == nx){//시작 -> 과정 -> 시작
+                    ans.push_back(i);
+                    break;
+                }
+                if(cx == nx){//i == nx가 아닌 경우 -> 잘못된 싸이클 형성
+                    
+                }
+            }
+        }
+    }
     return 0;
 }
