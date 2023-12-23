97b45dd0a0ab91344eb8a35fc8d9ccb46ba7f139
hyeon23 <urim2270@naver.com>
2023-12-23 19:17:20 +0900

    boj update

--- c++/VSCodeCodingTest/18511.cpp
+++ c++/VSCodeCodingTest/18511.cpp
@@ -1,32 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int N, K, ans;
-vector<int> v;
-
-void dfs(int num, int ten){
-    if(num > N) return;
-
-    ans = max(ans, num);
-
-    for(int i = 0; i < v.size(); ++i)
-        dfs(num + ten * v[i], ten * 10);
-}
-
-int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    cin >> N >> K;
-
-    int input;
-
-    for(int i = 0; i < K; ++i){
-        cin >> input;
-        v.push_back(input);
+int N, K, tmp, ans = 0;
+vector<int> vec;
+void dfs(int a, int n){
+    if(a > N) return;
+    ans = max(ans, a);
+    for(int i = 0; i < vec.size(); ++i){
+        dfs(a + vec[i] * n, n * 10);
     }
-    sort(v.begin(), v.end()); // 1, 5, 7
-
+}
+int main(){
+    cin >> N >> K;
+    for(int i = 0; i < K; ++i){
+        cin >> tmp;
+        vec.push_back(tmp);
+    }
     dfs(0, 1);
-
-    cout << ans << '\n';
+    cout << ans;
+    return 0;
 }
