4937b76ce965ede08121409ca0335c9219622734
hyeon23 <urim2270@naver.com>
2023-12-25 00:14:41 +0900

    boj update

---
+++ c++/VSCodeCodingTest/9184.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, c;
+int w(int a, int b, int c){
+    if(a <= 0 || b <= 0 || c <= 0) return 1;
+    else if(a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+    else if(a < b && b < c) return w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
+    else return w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
+}
+int main(){
+    while(cin >> a >> b >> c){
+        if(a == -1 && b == -1 && c == -1) break;
+        printf("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c));
+    }
+    return 0;
+}
