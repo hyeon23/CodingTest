c0bc8484d6bb1698db161838d45314a31a6a4b81
hyeon23 <urim2270@naver.com>
2023-12-25 18:42:55 +0900

    boj update

---
+++ c++/VSCodeCodingTest/10997.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+int N;
+vector<vector<char>> board(1001, vector<char>(1001, ' '));
+void drawStar(int x, int y, int n){
+    //가로길이 = 4 * N - 3
+    //세로길이 = 가로길이 + 2(N != 1)
+    int width = 4 * n - 3;
+    int height = width + 2;
+    for(int i = 1; i < width; ++i) board[x][y--] = '*';
+    for(int i = 1; i < height; ++i) board[x++][y] = '*';
+    for(int i = 1; i < width; ++i) board[x][y++] = '*';
+    for(int i = 1; i < height - 2; ++i) board[x--][y] = '*';
+    board[x][y--] = '*';
+    board[x][y] = '*';
+    if(n == 2){
+        board[x][y-1] = '*';
+        board[x+1][y-1] = '*';
+        board[x+2][y-1] = '*';
+        return;
+    }
+    drawStar(x, y-1, n-1);
+}
+int main(){
+    cin >> N;
+    int width = 4 * N - 3;
+    int height = width + 2;
+    //N == 1일 땐, 따로 처리
+    if(N == 1){
+        cout << "*" << '\n';
+        return 0;
+    }
+    //재귀 시작점 = 최초 너비 - 1
+    int x = 0, y = 4 * N - 4;
+    drawStar(x, y, N);
+    for(int i = 0; i < height; ++i){
+        //하나 안하나 결과는 동일한 형태로 출력되지만, 문제가 이상해서 이렇게 줄넘김을 하지 않으면 정답으로 인정 X
+        if(i == 1){
+            cout << '*' << '\n';
+            continue;
+        }
+        for(int j = 0; j < width; ++j){
+            cout << board[i][j];
+        }
+        cout << '\n';
+    }
+    return 0;
+}
