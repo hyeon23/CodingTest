9dca55f354bf1531744db499ee58aba945d8c7c3
hyeon23 <urim2270@naver.com>
2024-01-04 00:44:48 +0900

    BOJ 12919 update

--- c++/VSCodeCodingTest/5639.cpp
+++ c++/VSCodeCodingTest/5639.cpp
@@ -1,6 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+int tree[10000];
+void postOrder(int start, int end){
+    if(start >= end) return;
+    if(start == end - 1){
+        cout << tree[start] << '\n';
+        return;
+    }
+    int idx = start + 1;
+    while(idx < end){
+        if(tree[start] < tree[idx]) break;
+        idx++;
+    }
+
+}
 int main(){
-    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int num;
+    int inputIdx = 0;
+    while(cin >> num){
+        tree[inputIdx++] = num;
+    }
+    postOrder(0, inputIdx);    
     return 0;
 }
