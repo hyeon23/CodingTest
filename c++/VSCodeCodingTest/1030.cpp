1ca6b372e4e4926bffcbc3d05cf9a547f92175c8
hyeon23 <urim2270@naver.com>
2024-01-08 13:23:56 +0900

    boj update 9997

--- c++/VSCodeCodingTest/1030.cpp
+++ c++/VSCodeCodingTest/1030.cpp
@@ -1,64 +1,55 @@
 /* 프렉탈 평면 */
-#include <iostream>
-#include <cmath>
+
+/* 
+아무리 복잡해보이는 프랙탈이라고 하더라도 처음에는 N * N으로 나눠지고 중간에 K * K만 검정색
+예제의 결과물이 27 x 27의 거대한 평면 프랙탈이더라도 시작은 3 X 3에 가운데만 검정색
+*/
+#include <bits/stdc++.h>
 #define WHITE 0
 #define BLACK 1
 using namespace std;
-int Time, N, K, R1, R2, C1, C2, Biggest;
-
-int getColor(int r, int c);
-bool is_middle(int r, int c, int);
+int Time, N, K, R1, R2, C1, C2, biggestFS;
+int getColor(int r, int c) {
+    int t_big = biggestFS;
+    int tr = r, tc = c, pr, pc;
+    while ((t_big /= N)!= 0) {            
+        // 주어진 좌표값이 N X N으로 나누어진 칸 중 어디에 속하는 지 파악
+        pr = tr / t_big;
+        pc = tc / t_big;
+        // 그 칸 안에서 K * K에 속하는 지 판단한다. 만약 속하면 BLACK을 return
+        if (is_middle(pr, pc, t_big / N) == true) {
+            return BLACK;
+        }
+        // 아니라면 계산의 대상을 그 칸 안으로 한정
+        tr -= (pr * t_big);
+        tc -= (pc * t_big);
+    }
+    return WHITE;
+}
+/*가운데 검정색인 K * K 영역을 판정하는 함수*/
+/*curFs : 현재 프렉탈 사각형의 길이*/
+bool is_middle(int r, int c, int curFS) {
+    int sp = (N - K) / 2;
+    int fp = sp + K;
+    if((sp <= r && r < fp) && (sp <= c && c < fp))
+        return true;
+    return false;
+}
 
 int main(void) {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
     cin >> Time >> N >> K >> R1 >> R2 >> C1 >> C2;
-
-    Biggest = pow(N, Time);             
+    /*FS : FractalSize*/
+    /*biggestFS : 가장 큰 프렉탈 사각형의 길이*/
     // 시간을 모두 진행했을 때 각 행과 열의 칸의 개수.
-    // 만약 N이 3이고 이 과정을 3번 진행했다면 전체 칸의 개수는 27이다 (3의 3제곱)
-    // 예제가 딱 그런 케이스 
-
+    biggestFS = pow(N, Time);
+    //출력을 희망하는 (i, j) 좌표에 대해 getColor(i, j) 함수를 통해 출력 여부 체크
     for (int i = R1; i <= R2; i++) {
         for (int j = C1; j <= C2; j++) {
             cout << getColor(i, j);
         }
         cout << '\n';
     }
-
     return 0;
-}
-
-/* 
-아무리 복잡해보이는 프랙탈이더라도 처음에는 N * N으로 나눠지고 중간에 K * K만 검정색
-즉 예제의 결과물이 27 x 27의 거대한 평면 프랙탈이라고 하더라도 시작은 3 X 3에 가운데만 검정색이었다.
-그러므로 우린 그 과정을 따라간다.*/
-int getColor(int r, int c) {
-    int t_big = Biggest;
-    int tr = r, tc = c, pr, pc;
-    while ((t_big /= N)!= 0) {            
-        // 주어진 좌표값이 N X N으로 나누어진 칸 중 어디에 속하는 지 파악한다. 
-        pr = tr / t_big;
-        pc = tc / t_big;
-
-        // 그 칸 안에서 K * K에 속하는 지 판단한다. 만약 속하면 BLACK을 return 한다. 
-        if (is_middle(pr, pc, t_big / N)) {
-            return BLACK;
-        }
-
-        // 아니라면 이제 계산의 대상을 그 칸 안으로 한정시킨다. 
-        tr -= (pr * t_big);
-        tc -= (pc * t_big);
-    }
-    return WHITE;
-}
-
-// 가운데에 있는 K * K의 검정색 영역을 판정하는 함수
-// t_big이 갱신되며 pr(r), pc(c)의 범위는 0 to 각 프렉탈의 한 변의 길이 - 1로 고정
-bool is_middle(int r, int c, int k) {  
-    int sp = (N - K) / 2;
-    int fp = sp + K;
-    if((sp <= r && r < fp) && (sp <= c && c < fp)) return true;
-    return false;
 }
--- c++/VSCodeCodingTest/Test.cpp
+++ c++/VSCodeCodingTest/Test.cpp
@@ -1,41 +1,55 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-// int main(){
-//     ios::sync_with_stdio(0);
-//     cin.tie(0); cout.tie(0);
-//     int N, L;
-//     cin >> N >> L;
-//     int x, idx, cur;
-//     for(int i = 0; i < N; ++i){
-//         cin >> x;
-//         pq.push({x, i});
-//         cur = pq.top().first;
-//         idx = pq.top().second;
-//         while(idx < max(0, i - L + 1)){
-//             pq.pop();
-//             cur = pq.top().first;
-//             idx = pq.top().second;
-//         }
-//         cout << cur << ' ';
-//     }
-//     return 0;
-// }
+/* 프렉탈 평면 */
 
+/* 
+아무리 복잡해보이는 프랙탈이라고 하더라도 처음에는 N * N으로 나눠지고 중간에 K * K만 검정색
+예제의 결과물이 27 x 27의 거대한 평면 프랙탈이더라도 시작은 3 X 3에 가운데만 검정색
+*/
 #include <bits/stdc++.h>
+#define WHITE 0
+#define BLACK 1
 using namespace std;
-deque<pair<int, int>> deq;
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int N, L, num;
-    cin >> N >> L;
-    for(int i = 0; i < N; ++i){
-        cin >> num;
-        while(!deq.empty() && deq.back().second >= num) deq.pop_back();
-        deq.push_back({i, num});
-        if(deq.front().first <= i - L) deq.pop_front();
-        cout << deq.front().second << ' ';
+int Time, N, K, R1, R2, C1, C2, biggestFS;
+int getColor(int r, int c) {
+    int t_big = biggestFS;
+    int tr = r, tc = c, pr, pc;
+    while ((t_big /= N)!= 0) {            
+        // 주어진 좌표값이 N X N으로 나누어진 칸 중 어디에 속하는 지 파악
+        pr = tr / t_big;
+        pc = tc / t_big;
+        // 그 칸 안에서 K * K에 속하는 지 판단한다. 만약 속하면 BLACK을 return
+        if (is_middle(pr, pc, t_big / N) == true) {
+            return BLACK;
+        }
+        // 아니라면 계산의 대상을 그 칸 안으로 한정
+        tr -= (pr * t_big);
+        tc -= (pc * t_big);
+    }
+    return WHITE;
+}
+/*가운데 검정색인 K * K 영역을 판정하는 함수*/
+/*curFs : 현재 프렉탈 사각형의 길이*/
+bool is_middle(int r, int c, int curFS) {
+    int sp = (N - K) / 2;
+    int fp = sp + K;
+    if((sp <= r && r < fp) && (sp <= c && c < fp))
+        return true;
+    return false;
+}
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> Time >> N >> K >> R1 >> R2 >> C1 >> C2;
+    /*FS : FractalSize*/
+    /*biggestFS : 가장 큰 프렉탈 사각형의 길이*/
+    // 시간을 모두 진행했을 때 각 행과 열의 칸의 개수.
+    biggestFS = pow(N, Time);
+    //출력을 희망하는 (i, j) 좌표에 대해 getColor(i, j) 함수를 통해 출력 여부 체크
+    for (int i = R1; i <= R2; i++) {
+        for (int j = C1; j <= C2; j++) {
+            cout << getColor(i, j);
+        }
+        cout << '\n';
     }
     return 0;
 }
