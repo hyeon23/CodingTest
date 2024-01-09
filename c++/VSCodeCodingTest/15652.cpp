b7cdceda8078b83f7079f77e4bd40742ef0c32ce
hyeon23 <urim2270@naver.com>
2024-01-10 03:38:42 +0900

    boj update 15654 add & 15653 complete

--- c++/VSCodeCodingTest/15652.cpp
+++ c++/VSCodeCodingTest/15652.cpp
@@ -29,6 +29,23 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+int N, M;
+vector<int> vec(9);
+void DFS(int depth, int num){
+    if(depth == M){
+        for(int i = 0; i < M; ++i){
+            cout << vec[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = num; i <= N; ++i){
+        vec[depth] = i;
+        DFS(depth+1, i);
+    }
+}
 int main(){
+    cin >> N >> M;
+    DFS(0, 1);
     return 0;
 }
