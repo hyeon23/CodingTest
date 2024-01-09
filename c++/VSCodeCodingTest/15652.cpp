e06d4d5a8fb39d77757eefd113db1a4d0b29603f
hyeon23 <urim2270@naver.com>
2024-01-10 02:53:24 +0900

    boj complete 15651 & update 15652

--- c++/VSCodeCodingTest/15652.cpp
+++ c++/VSCodeCodingTest/15652.cpp
@@ -1,28 +1,34 @@
-#include <iostream>
-#define MAX 9
+// #include <iostream>
+// #define MAX 9
+// using namespace std;
+
+// int n, m;
+// int arr[MAX];
+
+// void DFS(int depth, int num)
+// {
+//     if (depth == m)
+//     {
+//         for (int i = 0; i < m; ++i)
+//             cout << arr[i] << ' ';
+//         cout << '\n';
+//         return;
+//     }
+
+//     for(int i = num; i <= n; ++i){
+//         arr[depth] = i;
+//         DFS(depth + 1, i);
+//     }
+// }
+
+// int main(){
+//     cin >> n >> m;
+//     DFS(0, 1);
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
-
-int n, m;
-int arr[MAX];
-
-void DFS(int depth, int num)
-{
-    if (depth == m)
-    {
-        for (int i = 0; i < m; ++i)
-            cout << arr[i] << ' ';
-        cout << '\n';
-        return;
-    }
-
-    for(int i = num; i <= n; ++i){
-        arr[depth] = i;
-        DFS(depth + 1, i);
-    }
-}
-
 int main(){
-    cin >> n >> m;
-    DFS(0, 1);
     return 0;
 }
