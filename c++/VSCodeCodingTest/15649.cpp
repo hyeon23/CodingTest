e2e69a588221e4a9d7c65068da13373a1f8fafee
hyeon23 <urim2270@naver.com>
2024-01-09 22:57:59 +0900

    boj update 15649

--- c++/VSCodeCodingTest/15649.cpp
+++ c++/VSCodeCodingTest/15649.cpp
@@ -1,61 +1,67 @@
-#include <iostream>
-#define MAX 9
-using namespace std;
-
-int n, m;
-int arr[MAX];//정렬된 순열을 담는 배열
-bool visited[MAX];//이미 방문된 숫자인지 판별하는 bool 배열
-
-void DFS(int depth){
-    if(depth == m){
-        for(int i = 0; i < m; ++i){
-            cout << arr[i] << " ";
-        }
-        cout << '\n';
-        return;
-    }
-    for(int i = 1; i <= n; ++i){
-        if(!visited[i]){
-            visited[i] = true;
-            arr[depth] = i;
-            DFS(depth + 1);
-            visited[i] = false;
-        }
-    }
-}
-
-int main() {
-  cin >> n >> m;
-  DFS(0);//DEPTH 0부터 끝까지 탐색
-}
-
 // #include <iostream>
-// #include <vector>
-// #include <algorithm>
-
 // #define MAX 9
 // using namespace std;
-// int N, M;
-// vector<int> graph;
 
-// int main()
-// {
-//     cin >> N >> M;
-//     // Graph 생성
-//     for (int i = 1; i <= N; ++i)
-//     {
-//         graph.push_back(i);
-//     }
+// int n, m;
+// int arr[MAX];//정렬된 순열을 담는 배열
+// bool visited[MAX];//이미 방문된 숫자인지 판별하는 bool 배열
 
-//     do
-//     {
-//         for (int i = 0; i < M; ++i)
-//         {
-//             cout << graph[i] << " ";
+// void DFS(int depth){
+//     if(depth == m){
+//         for(int i = 0; i < m; ++i){
+//             cout << arr[i] << " ";
 //         }
-//         cout << "\n";
-//         reverse(graph.begin() + M, graph.end());
-//     } while (next_permutation(graph.begin(), graph.end()));
-
-//     return 0;
+//         cout << '\n';
+//         return;
+//     }
+//     for(int i = 1; i <= n; ++i){
+//         if(!visited[i]){
+//             visited[i] = true;
+//             arr[depth] = i;
+//             DFS(depth + 1);
+//             visited[i] = false;
+//         }
+//     }
 // }
+
+// int main() {
+//   cin >> n >> m;
+//   DFS(0);//DEPTH 0부터 끝까지 탐색
+// }
+
+// // #include <iostream>
+// // #include <vector>
+// // #include <algorithm>
+
+// // #define MAX 9
+// // using namespace std;
+// // int N, M;
+// // vector<int> graph;
+
+// // int main()
+// // {
+// //     cin >> N >> M;
+// //     // Graph 생성
+// //     for (int i = 1; i <= N; ++i)
+// //     {
+// //         graph.push_back(i);
+// //     }
+
+// //     do
+// //     {
+// //         for (int i = 0; i < M; ++i)
+// //         {
+// //             cout << graph[i] << " ";
+// //         }
+// //         cout << "\n";
+// //         reverse(graph.begin() + M, graph.end());
+// //     } while (next_permutation(graph.begin(), graph.end()));
+
+// //     return 0;
+// // }
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    return 0;
+}
