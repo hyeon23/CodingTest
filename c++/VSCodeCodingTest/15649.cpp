7bfb3384173f1286ee1578ff582c79c0f8eee2ec
hyeon23 <urim2270@naver.com>
2024-01-10 00:45:42 +0900

    boj update 15650

--- c++/VSCodeCodingTest/15649.cpp
+++ c++/VSCodeCodingTest/15649.cpp
@@ -62,6 +62,27 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+int N, M;
+vector<int> arr(9, 0);
+vector<bool> visited(9, false);
+void DFS(int n){
+    if(n == M){
+        for(int i = 0; i < M; ++i){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 1; i <= N; ++i){
+        if(!visited[i]){
+            visited[i] = true;
+            arr[n] = i;
+            DFS(n+1);
+            visited[i] = false;
+        }
+    }
+}
 int main(){
-    return 0;
+    cin >> N >> M;
+    DFS(0);
 }
