583b6db97f01e3e348e53ed9fd6e0c19526ceaf3
hyeon23 <urim2270@naver.com>
2024-01-10 02:40:53 +0900

    boj update

--- c++/VSCodeCodingTest/15650.cpp
+++ c++/VSCodeCodingTest/15650.cpp
@@ -35,6 +35,30 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+int N, M;
+vector<int> vec(9, 0);
+vector<bool> visited(9, false);
+void DFS(int depth, int num){
+    if(depth == M){
+        for(int i = 0; i < M; ++i){
+            cout << vec[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = num; i <= N; ++i){
+        if(!visited[i]){
+            visited[i] = true;
+            vec[depth] = i;
+            DFS(depth+1, i+1);
+            visited[i] = false;
+        }
+    }
+}
 int main(){
+    //1 to N 중복 비허용 M개
+    //오름차순
+    cin >> N >> M;
+    DFS(0, 1);
     return 0;
 }
