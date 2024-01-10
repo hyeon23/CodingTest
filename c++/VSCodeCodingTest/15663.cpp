3c3652eb273e6de4df1fde8cc5b15522fd90f51e
hyeon23 <urim2270@naver.com>
2024-01-10 17:23:01 +0900

    boj update 15663 complete

--- c++/VSCodeCodingTest/15663.cpp
+++ c++/VSCodeCodingTest/15663.cpp
@@ -3,17 +3,31 @@ using namespace std;
 int N, M;
 vector<int> input;
 vector<int> vec(9);
-void DFS(int depth, int num){
+vector<bool> visited(9);
+// set<vector<int>> ans;
+//중복되는 수열 출력 불가
+//사전 순으로 증가
+void DFS(int depth){
     if(depth == M){
-        for(int i = 0; i < M; ++i){
-            cout << vec[i] << ' ';
+        for (int i = 0; i < M; ++i) {
+          cout << vec[i] << ' ';
         }
         cout << '\n';
+        // vector<int> anstmp;
+        // for(int i = 0; i < M; ++i)
+        //     anstmp.push_back(vec[i]);
+        // ans.insert(anstmp);
         return;
     }
-    for(int i = num; i < N; ++i){
-        vec[depth] = input[i];
-        DFS(depth+1, i);
+    int tmp = 0;
+    for(int i = 0; i < N; ++i){
+        if(!visited[i] && tmp != input[i]){
+            visited[i] = true;
+            vec[depth] = input[i];
+            tmp = vec[depth];
+            DFS(depth+1);
+            visited[i] = false;
+        }
     }
 }
 int main(){
@@ -24,6 +38,12 @@ int main(){
         input.push_back(tmp);
     }
     sort(input.begin(), input.end());
-    DFS(0, 0);
+    DFS(0);
+    // for(auto s : ans){
+    //     for(auto i : s){
+    //         cout << i << ' ';
+    //     }
+    //     cout << '\n';
+    // }
     return 0;
 }
