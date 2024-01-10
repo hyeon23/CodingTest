4535859a089be5ab98f114acff380cc57f515c9b
hyeon23 <urim2270@naver.com>
2024-01-11 03:49:59 +0900

    1759 Complete & 9663 N_Queen Named Complete

--- c++/VSCodeCodingTest/1759.cpp
+++ c++/VSCodeCodingTest/1759.cpp
@@ -2,19 +2,17 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 int L, C;
 vector<char> v;
-vector<char> res;
-
+string curstr;
 bool check(){
     int moum = 0;
     for(int i = 0; i < L; ++i){
-        if(res[i] == 'a' ||
-        res[i] == 'e' ||
-        res[i] == 'i' ||
-        res[i] == 'o' ||
-        res[i] == 'u') 
+        if(curstr[i] == 'a' ||
+        curstr[i] == 'e' ||
+        curstr[i] == 'i' ||
+        curstr[i] == 'o' ||
+        curstr[i] == 'u') 
         moum++;
     }
     //모음 수 1개 이상, 자음 수 = 전체 수 - 모음 수 = 2개 이상
@@ -23,25 +21,22 @@ bool check(){
     else
         return false;
 }
-
 void DFS(int N){
-    if((int)res.size() == L){
+    if((int)curstr.size() == L){
         if(check()){
             for(int i = 0; i < L; ++i){
-                cout << res[i];
+                cout << curstr[i];
             }
             cout << '\n';
         }
         return;        
     }
-
     for(int i = N; i < C; ++i){
-        res.push_back(v[i]);//들어갈 때, 추가
+        curstr.push_back(v[i]);//들어갈 때, 추가
         DFS(i + 1);
-        res.pop_back();//나올 때, 빼기
+        curstr.pop_back();//나올 때, 빼기
     }
 }   
-
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
