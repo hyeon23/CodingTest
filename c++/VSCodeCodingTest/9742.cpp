a90439962ba599944a5776cc89864e647a8cd083
hyeon23 <urim2270@naver.com>
2024-02-02 13:10:55 +0900

    boj 21375 complete

--- c++/VSCodeCodingTest/9742.cpp
+++ c++/VSCodeCodingTest/9742.cpp
@@ -1,22 +1,64 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int N;
+// string str;
+// int main(){
+//     ios::sync_with_stdio(0);
+//     cin.tie(0); cout.tie(0);
+//     while(cin >> str >> N){
+//         int cnt = 0;
+//         bool trigger = false;
+//         string tmp = str;
+//         while(cnt < N-1){
+//             ++cnt;
+//             if(!next_permutation(str.begin(), str.end())){
+//                 trigger = true;
+//                 break;
+//             }
+//         }
+//         if(trigger) cout << tmp << ' ' << N << " = " << "No permutation" << '\n';
+//         else cout << tmp << ' ' << N << " = " << str << '\n';
+//     }
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
-int N;
+int N, cnt = 0;
 string str;
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    while(cin >> str >> N){
-        int cnt = 0;
-        bool trigger = false;
-        string tmp = str;
-        while(cnt < N-1){
-            ++cnt;
-            if(!next_permutation(str.begin(), str.end())){
-                trigger = true;
-                break;
+bool tri = false;
+vector<char> vec(11);
+vector<bool> visited(11);
+void DFS(int depth){
+    if(depth == str.size()){
+        ++cnt;
+        if(cnt == N){
+            string tmp = "";
+            for(int i = 0; i < str.size(); ++i){
+                tmp += vec[i];
+            }
+            if(tmp > str) {
+                tri = true;
+                cout << str << ' ' << N << " = " << tmp << '\n';
             }
         }
-        if(trigger) cout << tmp << ' ' << N << " = " << "No permutation" << '\n';
-        else cout << tmp << ' ' << N << " = " << str << '\n';
+        return;
     }
+    for(int i = 0; i < str.size(); ++i){
+        if(visited[i]) continue;
+        visited[i] = true;
+        vec[depth] = str[i];
+        DFS(depth+1);
+        visited[i] = false;
+    }
+}
+int main(){
+    while(cin >> str >> N){
+        cnt = 0;
+        tri = false;
+        vec.clear();
+        visited.clear();
+        DFS(0);
+        if(!tri) cout << str << ' ' << N << " = " << "No permutation" << '\n';
+    }
+    return 0;
 }
