aca28ad413f18476ccdfa2888040412af23af2c4
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-05-21 12:55:36 +0900

    boj update

--- c++/jejongSW/pro1.cpp
+++ c++/jejongSW/pro1.cpp
@@ -1,13 +1,30 @@
-#include <iostream>
+#include <map>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+
 using namespace std;
 
-// int main()
-// {
-//     int N, max;
-//     cin >> N;
-//     for(int i = 1; i <= N; ++i)
-//         max += i;
-//     cout << max;
-//     return 0;
-// }
+vector<int> solution(vector<string> id_list, vector<string> report, int k) {
+    // 1.
+    const int n = id_list.size();
+    map<string, int> Conv;
+    for (int i = 0; i < n; i++) Conv[id_list[i]] = i;
+
+    // 2.
+    vector<pair<int, int>> v;
+    sort(report.begin(), report.end());
+    report.erase(unique(report.begin(), report.end()), report.end());
+    for (const auto& s : report) {
+        stringstream k(s);
+        string a, b; k >> a >> b;
+        v.push_back({ Conv[a], Conv[b] });
+    }
+
+    // 3.
+    vector<int> cnt(n), ret(n);
+    for (const auto& [a, b] : v) cnt[b]++;
+    for (const auto& [a, b] : v) if (cnt[b] >= k) ret[a]++;
+    return ret;
+}
