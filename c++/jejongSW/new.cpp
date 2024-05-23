21789de46f8ea14e094fed955de0550d2a73f15a
Hyeon <77566434+hyeon23@users.noreply.github.com>
2024-05-23 13:38:58 +0900

    boj updtae

--- c++/jejongSW/new.cpp
+++ c++/jejongSW/new.cpp
@@ -7,23 +7,12 @@
 using namespace std;
 
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    bool arr[5][5] = {false,};
 
-    int N, Q, L, R;
-    cin >> N >> Q;
-
-    vector<int> A(N+1);
-
-    for(int i = 1; i <= N; ++i) cin >> A[i];
-
-    sort(A.begin(), A.end());
-
-    partial_sum(A.begin(), A.end(), A.begin());
-
-    for(int i = 0; i < Q; ++i){
-        cin >> L >> R;
-        cout << A[R] - A[L-1] << '\n';
+    for(int i = 0; i < 5; ++i){
+        for(int j = 0; j < 5; ++j){
+            cout << arr[i][j] << ' ';
+        }
+        cout << '\n';
     }
-    return 0;
 }
